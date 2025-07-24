5ad5f72aa8c9ca9b31e5e1dfe6fff34b84dcf687
Fadhlan <fadhlanzap@gmail.com>
2025-07-24 17:54:49 +0700

    add map

--- fe/app/peta-budaya/page.tsx
+++ fe/app/peta-budaya/page.tsx
@@ -16,17 +16,24 @@ const PetaBudaya = () => {
       <h1 className="font-bold text-3xl">Peta Budaya</h1>
       <div className="rounded-lg gap-10 p-3 shadow-md space-y-4">
         <Map />
-        <div className="flex gap-4">
-          <input
-            placeholder="Cari daerah ..."
-            className="w-full border-2 border-[#FF7D29] px-5 rounded-full py-2"
-          />
-          <div>
+        <div className="flex gap-20 justify-between">
+          <div className="relative rounded-full w-full h-12">
+            <input
+              placeholder="Cari daerah ..."
+              className=" border-2 border-[#FF7D29] px-5 rounded-full h-full w-full"
+            />
+            <button className="bg-[#FF7D29] absolute right-2 top-2 rounded-full h-8 w-8 flex items-center justify-center">
+              <IoMdSearch className="size-5 text-white" />
+            </button>
+          </div>
+          <div className="flex items-center justify-center gap-10">
             {categoryButton.map((category) => (
               <button
                 key={category}
                 onClick={() => setIsActive(category)}
-                className={`${isActive ? "bg-[#FF7D29]" : "border border-[#1918251A]"} p-2`}
+                className={`${
+                  isActive === category ? "bg-[#FF7D29] text-white" : "border border-[#1918251A]"
+                } px-4 py-2 m-0 rounded-full h-full text-nowrap`}
               >
                 {category}
               </button>
